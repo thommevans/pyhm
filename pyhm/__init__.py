dae16d137446381dbdc5fff659f137905ba74a12
tomevans <tom.evans@astro.ox.ac.uk>
2013-11-12 17:40:57 +0000

    Test commit to see if git will still update changes to pyhm/__init__.py

--- pyhm/__init__.py
+++ pyhm/__init__.py
@@ -5,3 +5,4 @@ import Optimizers
 from InstantiationDecorators import stochastic
 from Utils import load_chain, combine_chains
 from SampleDiagnostics import plot_running_chain_means, plot_chain_traces, plot_chain_densities, plot_chain_autocorrs, print_chain_properties, gelman_rubin
+# this is a test comment
