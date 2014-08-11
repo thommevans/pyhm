59e18cccfc6eceda38960f65fc2321a7f0a5fd47
tomevans <tom.evans@astro.ox.ac.uk>
2014-08-11 18:26:11 +0100

    Totally revised the nestedsampling_temp.py script

--- pyhm/nestedsampling_temp.py
+++ pyhm/nestedsampling_temp.py
@@ -1,14 +1,11 @@
 import numpy as np
 import scipy.linalg
-import pdb, os, sys, time
 import matplotlib.pyplot as plt
+import pdb, os, sys
 
-# The discarded points make up the posterior, but need to be weighted by L*w/Z.
-# Then perhaps you use that to calculate the weighted mean. Then you can probably
-# just to really simple numerical integration away from the mean for each parameter
-# to marginalise over +/-34% etc. Need to figure out how this works properly...
+##########################################################################################
 
-
+# Generate a toy dataset with linear trend:
 a = 2.0
 b = 1.0
 ndat = 1000
@@ -16,28 +13,26 @@ x = np.r_[ -10:10:1j*ndat ]
 unc = 1
 y = a*x + b + unc*np.random.randn( ndat )
 
-# Generate an active set from the prior.
-nactive = 30
-a_sample = 1000*( -0.5 + np.random.random( nactive ) )
-b_sample = 1000*( -0.5 + np.random.random( nactive ) )
-A = np.row_stack( [ a_sample, b_sample ] )
 
-def loglikelihood( a, b ):
+def logL( a, b ):
+    """
+    Given parameter values a and b, calculates
+    the log likelihood.
+    """
     model = a*x + b
     resids = ( y - model ).flatten()
     n = len( resids )
-    logp = -n*np.log( unc ) - 0.5*n*np.log( 2*np.pi ) - 0.5*np.sum( ( resids/unc )**2. )
-    return logp
+    return -n*np.log( unc ) - 0.5*n*np.log( 2*np.pi ) - 0.5*np.sum( ( resids/unc )**2. )
 
-def sample( X, logp_min ):
+def sample_ellipsoid( V, logp_min ):
     """
-    Given X, where each row is a different input vector, this
+    Given V, where each row is a different input vector, this
     routine will take a random sample from the ellipsoid.
     """
 
-    d, n = np.shape( X )
-    mu = np.matrix( np.reshape( np.mean( X, axis=1 ), [ d, 1 ] ) )
-    C = np.matrix( np.cov( X ) )
+    d, n = np.shape( V )
+    mu = np.matrix( np.reshape( np.mean( V, axis=1 ), [ d, 1 ] ) )
+    C = np.matrix( np.cov( V ) )
     eigvals, R = np.linalg.eig( C )
     M = np.matrix( np.sqrt( np.diag( eigvals ) ) )
 
@@ -45,12 +40,12 @@ def sample( X, logp_min ):
     L = np.array( np.linalg.cholesky( C ) )
     k = np.zeros( n )
     for i in range( n ):
-        r = np.array( X[:,i] ).flatten() - np.array( mu ).flatten()
+        r = np.array( V[:,i] ).flatten() - np.array( mu ).flatten()
         B = scipy.linalg.lu_solve( scipy.linalg.lu_factor( L ), r )
         B = np.reshape( B, [ d, 1 ] )
         k[i] = float( np.matrix( B ).T*np.matrix( B ) )
 
-    f = 1.06
+    f = 1.06 # ellipsoid expansion factor
     T = f*np.sqrt( k.max() )*( R.T*M*R )
 
     logp_new = logp_min - 1
@@ -60,70 +55,108 @@ def sample( X, logp_min ):
         u = np.random.random()
         z = ( u**( 1./d ) )*w
         z = np.matrix( np.reshape( z, [ d, 1 ] ) )
-        y = np.array( T*z + mu ) # this is the random sample drawn uniformly from the ellipsoid
-        logp_new = loglikelihood( y[0], y[1] )
-        
+        y = np.array( T*z + mu ).flatten() # this is the random sample drawn uniformly from the ellipsoid
+        logp_new = logL( y[0], y[1] )
+
     return y, logp_new
 
+########################################################################
+# Sampling.
 
-# 1. Draw N samples randomly from the prior over the model's parameter space (the "active set").
-# 2. Remove the sample from the active set that has the lowest likelihood L0.
-# 3. Add a new random sample to the active set subject to the restriction that its likelihood be L>L0.
-# 4. Iterate over steps 2 and 3 until some stopping criterion achieved.
-# 5. Compute the evidence by integrating the sorted likelihoods that were discarded at each iteration in step 2.
+# Define the sampling parameters:
+nactive = 100 # number of samples to generate (i.e. number of points in the active sample)
+npar = 2 # dimensionality of the parameter space (i.e. the 'a' and 'b' parameters)
 
+# Generate the active set:
+a_active = 5*( -0.5 + np.random.random( nactive ) )
+b_active = 5*( -0.5 + np.random.random( nactive ) )
+A_active = np.row_stack( [ a_active, b_active ] )
 
-
-# Compute the likelihoods of the active set samples.
-
-logL_active =  np.zeros( nactive )
+# Evaluate the log likelihoods of the active set points:
+logL_active = np.empty( nactive )
 for i in range( nactive ):
-    logL_active[i] = loglikelihood( A[0,i], A[1,i] )
+    logL_active[i] = logL( A_active[0,i], A_active[1,i] )
 
-nsamples = 5000
-w = np.zeros( nsamples )
-logL_chain = np.zeros( nsamples )
-a_chain = np.zeros( nsamples )
-b_chain = np.zeros( nsamples )
-X = np.zeros( nsamples )
-w = np.zeros( nsamples-1 )
-logZ = np.zeros( nsamples-1 )
-kappa = 0.1
-for i in range( nsamples ):
-
-    # Remove lowest likelihood point from active set.
-    ix = np.argmin( logL_active )
-    logL_chain[i] = logL_active[ix]
-    a_chain[i] = A[0,ix]
-    b_chain[i] = A[1,ix]
+def log_add( loga, logc ):
+    """
+    Given log(a) and log(c), returns log(a+c).
+    """
+    if loga>logc:
+        return loga + np.log( 1+np.exp( logc-loga ) )
+    else:
+        return logc + np.log( 1+np.exp( loga-logc ) )
     
-    # Generate a new sample point.
-    A = np.column_stack( [ A[:,:ix], A[:,ix+1:] ] )
-    logL_active = np.concatenate( [ logL_active[:ix], logL_active[ix+1:] ] )
-    Anew, logL_new = sample( A, logL_active[ix] )
-    A = np.column_stack( [ A, Anew ] )
-    logL_active = np.concatenate( [ logL_active, [ logL_new ] ] )
+logL_chain = []
+A_chain = []
+X_chain = []
+w_chain = []
+logZ = -sys.float_info.max
+i = 0
+fstop = 0.05
+terminate = False
+while terminate==False:
+    ix = np.argmin( logL_active )
+    logL_chain += [ logL_active[ix] ]
+    A_chain += [ A_active[:,ix] ]
+    X_chain += [ np.exp( -float( i )/float( nactive ) ) ]
+    if i>1:
+        w_chain += [ 0.5*( X_chain[i-2] - X_chain[i] ) ]
+        delta_logZ = logL_chain[i-1] + np.log( w_chain[-1] )
+        logZ = log_add( logZ, delta_logZ )
 
-    X[i] = ( float( nactive )/float( nactive+1 ) )**( i+1 )
-    if i>0:
-        w[i-1] = X[i-1]-X[i] # currently this is not strictly a correct implementation of the trapezoid rule;
-                             # will be a simple adjustment to correct this, including overshoot conditions
-                             # suggested in Sivia & Skilling
-        logL_max = logL_chain[:i+1].max()
-        logL_chain_normalised = logL_chain[:i+1] - logL_max
-        L_chain_normalised = np.exp( logL_chain_normalised )
-        Z_normalised = 0.5*np.sum( w[:i]*L_chain_normalised[1:i+1] )
-        logZ[i-1] = logL_max + Z_normalised #-logL_max + Z_normalised
-        print i+1, X[i], logZ[i-1], logZ[i-1]-logZ[i-2]
-        if i>100:
-            # Stopping criterion:
-            if logL_active.max() + np.log( X[i] ) - logZ[i-1] < np.log( kappa ):
-                print 'Stopping criterion met - breaking out'
-                break
-    #if i>1:
-    #    LHS = logL_active.max() + np.log( X[i] ) - logZ[i]
-    #    RHS = np.log( 1+np.exp( kappa ) )
-    #    print i+1, LHS, RHS
-    #    if LHS<RHS:
-    #        break
+        # Stopping criterion:
+        if ( logL_active.max() + np.log( X_chain[i] ) - logZ < np.log( fstop ) ):
+            terminate = True
+        
+    i += 1
+    # Replace the lowest likelihood point from the active
+    # with a new draw from the ellipsoid:
+    A_active = np.column_stack( [ A_active[:,:ix], A_active[:,ix:] ] )
+    logL_i = logL_active[ix]
+    A_new, logL_new = sample_ellipsoid( A_active, logL_i )
+    A_active[:,ix] = A_new
+    logL_active[ix] = logL_new
+    
+logL_chain = np.array( logL_chain )
+A_chain = np.column_stack( A_chain ).T
+X_chain = np.array( X_chain )
+w_chain = np.array( w_chain )
 
+nsamples = len( logL_chain )
+log_percentiles = np.zeros( [ nsamples-2, npar ] )
+for i in range( npar ):
+    ixs = np.argsort( A_chain[1:-1,i] )
+    running_log_count = -sys.float_info.max
+    for j in range( nsamples-2 ):
+        print i, j
+        delta_log_count = logL_chain[1:-1][ixs][j] + np.log( w_chain[ixs][j] )
+        running_log_count = log_add( running_log_count, delta_log_count )
+        log_percentiles[j,i] = running_log_count - logZ
+percentiles = np.exp( log_percentiles )
+
+def par_range( par_chain, percentiles, plusminus ):
+
+    ix_med = np.argmin( np.abs( percentiles-0.5 ) )
+    ix_low = np.argmin( np.abs( percentiles-(0.5-plusminus) ) )
+    ix_upp = np.argmin( np.abs( percentiles-(0.5+plusminus) ) )    
+
+    median = par_chain[ix_med]
+    low = par_chain[ix_low]
+    upp = par_chain[ix_upp]
+    
+    return median, low, upp
+
+medians = np.zeros( npar )
+for k in range( npar ):
+    median, low, upp = par_range( A_chain[:,k], percentiles[:,k], 0.34 )
+    print median, median-low, upp-median
+    medians[k] = median
+bests = np.zeros( npar )
+ix = np.argmax( logL_chain )
+bests[0] = A_chain[ix,0]
+bests[1] = A_chain[ix,1]
+
+plt.figure()
+plt.plot( x, y, '.k' )
+plt.plot( x, medians[0]*x + medians[1], '-r' )
+plt.plot( x, bests[0]*x + bests[1], '-b' )
