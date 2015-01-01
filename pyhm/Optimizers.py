f0c400cf8b1e2c39285c3231271d4517c9286440
tomevans <tom.evans@astro.ox.ac.uk>
2015-01-01 23:12:11 +0000

    Have improved the Hessian routine in the Optimizers.py module, and also the subsequent estimate of the covariance matrix.

--- pyhm/Optimizers.py
+++ pyhm/Optimizers.py
@@ -12,7 +12,7 @@ except:
 This module defines the optimization algorithms for MAP objects.
 """
 
-def optimize( MAP, method='neldermead', maxfun=10000, maxiter=10000, ftol=None, verbose=False ):
+def optimize( MAP, method='neldermead', maxfun=10000, maxiter=10000, ftol=None, xtol=None, verbose=False, epsilon=1e-5 ):
     """
     Wrapper for the scipy optimizers.
 
@@ -75,14 +75,16 @@ def optimize( MAP, method='neldermead', maxfun=10000, maxiter=10000, ftol=None, 
     # Run the optimizer specified in the call:
     if method=='neldermead':
         if ftol==None:
-            ftol = 1e-6
-        ftol=1e-12
-        xtol=1e-12
+            ftol = 1e-8
+        if xtol==None:
+            xtol = 1e-8
         xopt = scipy.optimize.fmin( func, x0, xtol=xtol, ftol=ftol, maxiter=maxiter, maxfun=maxfun, \
                                     full_output=0, disp=verbose )
     elif method=='powell':
         if ftol==None:
-            ftol = 0.01
+            ftol = 1e-8
+        if xtol==None:
+            xtol = 1e-8
         xopt = scipy.optimize.fmin_powell( func, x0, ftol=ftol, maxiter=maxiter, full_output=0, disp=verbose )
     elif method=='conjgrad':
         if ftol!=None:
@@ -90,7 +92,7 @@ def optimize( MAP, method='neldermead', maxfun=10000, maxiter=10000, ftol=None, 
             warn_str = '\nConjugate gradient does not accept ftol (ignoring)\n'
             warnings.warn( warn_str )
             pdb.set_trace()
-        gtol = 1e-6 # stop when gradient less than this
+        gtol = 1e-8 # stop when gradient less than this
         xopt = scipy.optimize.fmin_cg( func, x0, maxiter=maxiter, full_output=0, disp=verbose, gtol=gtol )
     else:
         pdb.set_trace() # method not recognised
@@ -106,39 +108,37 @@ def optimize( MAP, method='neldermead', maxfun=10000, maxiter=10000, ftol=None, 
             if len( xopt_i )==1:
                 free_stochastics[keys[i]].value = float( xopt_i )
             else:
-                free_stochastics[keys[i]].value = np.array( xopt_i )                
+                free_stochastics[keys[i]].value = np.array( xopt_i )
 
-    n0=len(xopt)
-    hess = np.zeros( [n0,n0])
-    eps = 1e-8
-    import copy
-    for i in range(n0):
-        xx=copy.deepcopy(xopt)
-        xx[i] -= eps
-        f1=func(xx)
-        xx=copy.deepcopy(xopt)
-        xx[i] += eps
-        f2=func(xx)
-        xx=copy.deepcopy(xopt)
-        f3=func(xx)
-        hess[i,i]=(f1+f2-2*f3)/(eps**2.)
-        
-    MAP.logp_hess = hessian( func, xopt )
-    #try:
-    #    MAP.pcov = np.linalg.inv( -MAP.logp_hess )
-    #except:
-    #    MAP.pcov = np.inf
-    MAP.pcov = np.linalg.inv(MAP.logp_hess)
+    # Evaluate the Hessian matrix of logp at the location of the
+    # maximum likelihood parameter values:
+    MAP.logp_hess = hessian( func, xopt, epsilon=epsilon )
 
-    print MAP.pcov
+    # NOTE: Sometimes if the numerical approximation to the Hessian requires
+    # evaluating func outside the allowed parameter range, the corresponding
+    # column and row will be filled with infs/nans; filter these out so that
+    # when the inverse is taken to get the covariance matrix it doesn't
+    # become filled with nans; this is temporary, a better solution might be
+    # to estimate the Hessian properly with the allowed parameter ranges
+    # taken into account....
+    ixs = np.arange( nstoch )[np.isfinite( np.diag( MAP.logp_hess ) )==True]
+    y = np.linalg.inv( MAP.logp_hess[ixs,:][:,ixs] )
+    # Start with matrix of nans
+    MAP.pcov = np.nan*np.ones( [ nstoch, nstoch]  )
+    # Fill in columns/rows that have finite values:
+    for i in range( len( ixs ) ):
+        for j in range( len( ixs ) ):
+            MAP.pcov[ixs[i],ixs[j]] = y[i,j]
     return None
 
-def hessian( f, x0 ):
-    delta = (1e-7)
+def hessian( f, x0, epsilon=1e-5 ):
+    """
+    Numerically approximate the Hessian matrix using finite differencing.
+    """
     dim = len( x0 )
-    deltaVec = delta*np.abs(x0)
-    ixs = deltaVec<delta
-    deltaVec[ixs] = delta
+    deltaVec = epsilon*np.abs(x0)
+    ixs = deltaVec<epsilon
+    deltaVec[ixs] = epsilon
     deltaMat = np.diag(deltaVec)
     deltaVec = np.reshape(deltaVec,[dim,1])
     fx = f(x0)
@@ -146,87 +146,12 @@ def hessian( f, x0 ):
     for m in range(dim):
         deltaUse=deltaMat[m,:]
         Hess[m,m] = (-f(x0+2*deltaUse)+16*f(x0+deltaUse)-30*fx+16*f(x0-deltaUse)-f(x0-2*deltaUse))/12.
-        for n in range(m,dim):
+        for n in range(m+1,dim):
             delta1 = deltaMat[m,:]
             delta2 = deltaMat[n,:]
             Hess[n,m] = (f(x0+delta1+delta2)-f(x0+delta1-delta2)-f(x0-delta1+delta2)+f(x0-delta1-delta2))/4.
             Hess[m,n] = Hess[n,m]
-            print m, n, Hess[m,n], Hess[m,m]
     divid = np.dot(deltaVec,deltaVec.T)
-    Hess /= divid
+    Hess *= 1./divid
     return Hess
-
-def hessian_OLD2( f, x0 ):
-
-    n = len( x0 )
-    #epsilon = np.sqrt( (1e-7)*np.abs(x0) )
-    epsilon = (1e-8)*np.abs(x0)
-    test = scipy.optimize.approx_fprime(x0,f,epsilon)
-    hess = np.zeros( [ n, n ] )
-    x = x0
-    for i in range( n ):
-        h = epsilon[i]
-        for j in range( n ):
-            k = epsilon[j]
-            if i==j:
-                x = x0
-                x[i] += h
-                f1 = f(x)
-                x = x0
-                f2 = f(x)
-                x = x0
-                x[i] -= h
-                f3 = f(x)
-                d2 = (f1-2*f2+f3)/(h**2.)
-            else:
-                x = x0
-                x[i] += h
-                x[j] += k
-                f1 = f(x)
-                x = x0
-                x[i] += h
-                x[j] -= k
-                f2 = f(x)
-                x = x0
-                x[i] -= h
-                x[j] += k
-                f3 = f(x)
-                x = x0
-                x[i] -= h
-                x[j] -= k
-                f4 = f(x)
-                d2 = (f1-f2-f3+f4)/(4*h*k)
-            print x0
-            hess[i,j] = d2
-    pdb.set_trace()
-    return hess
-
-def hessian_OLD( f, x0, epsilon=1.e-8, linear_approx=False ):
-    """
-    A numerical approximation to the Hessian matrix of cost function at
-    location x0 (hopefully, the minimum). THIS NEEDS TO BE CHECKED!!!!
-    """
-    # ``calculate_cost_function`` is the cost function implementation
-    # The next line calculates an approximation to the first
-    # derivative
-    f1 = scipy.optimize.approx_fprime( x0, f, epsilon )
-
-    # This is a linear approximation. Obviously much more efficient
-    # if cost function is linear
-    if linear_approx:
-        f1 = np.matrix(f1)
-        return f1.transpose() * f1
-    # Allocate space for the hessian
-    n = x0.shape[0]
-    hessian = np.zeros ( ( n, n ) )
-    # The next loop fill in the matrix
-    xx = x0
-    for j in xrange( n ):
-        xx0 = xx[j] # Store old value
-        xx[j] = xx0 + epsilon # Perturb with finite difference
-        # Recalculate the partial derivatives for this new point
-        f2 = scipy.optimize.approx_fprime( x0, f, epsilon )
-        hessian[:, j] = (f2 - f1)/epsilon # scale...
-        xx[j] = xx0 # Restore initial value of x0
-    return hessian
 
